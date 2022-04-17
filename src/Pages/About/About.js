17b3247d66f99146c3af21061adeb5a4ed42754e
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-04-17 09:41:33 +0600

    added my personal data

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import pic1 from '../../images/pic-1.jpeg';
 
 const About = () => {
     return (
-        <div>
-            <h2 className='text-center'>This is about</h2>
+        <div className='text-center'>
+            <h2 className='text-center'>This is about me.</h2>
+            <img style={{width:'150px', height:'250px'}} src={pic1} alt="" />
+            <p>Name: Abdur Rahaman.</p>
+            <p>Goal: I want to be a developer with my hardwork.</p>
+            
         </div>
     );
 };
